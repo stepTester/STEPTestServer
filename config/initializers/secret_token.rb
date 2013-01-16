c8c6b3f307d7cb645a298054759ccddc6e70d7c7
Alex Hyde <ahyde@stepnetworks.com>
2013-01-16 11:45:01 -0500

    Git Commit test

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+STEPTestingServer::Application.config.secret_token = 'c63199ab91154b315ba29271605a7830c87f26ea5dd07e671235cb6ca14399f1eee0d86ef77705a8413bf87dc185b50819904a30b02660fef5a7bfdf8b7e79a2'
